b13c7917e7ac288c5ce8dcf748a3b5f27d0ad26f
ytj <tjyan2004@gmail.com>
2025-05-06 14:54:04 +0000

    feat: edit task title

--- components/copilot/CopilotHeader.vue
+++ components/copilot/CopilotHeader.vue
@@ -1,10 +1,56 @@
 <template>
-  <div class="px-4 h-16 min-h-16 max-h-16 flex items-center gap-2">
-    <h1 class="text-lg">
-      <div class="i-fa6-brands:internet-explorer text-lg inline-block mb-[-0.15rem]" />
-      {{ $t('webpilot.name') }}
+  <div class="px-4 h-16 min-h-16 max-h-16 flex items-center space-x-2">
+    <div class="i-fa6-brands:internet-explorer text-lg inline-block mb-[-0.15rem]" />
+    <h1 class="flex-1 text-lg overflow-hidden text-nowrap text-ellipsis">
+      {{ taskContext.title || $t('webpilot.name') }}
     </h1>
-    <div class="flex-1" />
+    <NPopconfirm
+      trigger="click"
+      placement="bottom"
+      :show-icon="false"
+      @positive-click="summarizeTask.execute()"
+    >
+      <template #trigger>
+        <NButton secondary circle class="!h-8 !w-8" :loading="summarizeTask.loading.value">
+          <template #icon>
+            <NIcon>
+              <div class="i-simple-icons:googlegemini" />
+            </NIcon>
+          </template>
+        </NButton>
+      </template>
+      {{ $t('webpilot.msg.summarize_task_confirm') }}
+    </NPopconfirm>
+    <NPopconfirm
+      v-model:show="showEditPopup"
+      trigger="manual"
+      placement="bottom"
+      :show-icon="false"
+      @positive-click="applyTitle"
+      @clickoutside="showEditPopup = false"
+    >
+      <template #trigger>
+        <NButton
+          secondary
+          circle
+          class="!h-8 !w-8"
+          :loading="saveTask.loading.value"
+          @click="toggleEditPopup"
+        >
+          <template #icon>
+            <NIcon>
+              <div class="i-carbon:edit" />
+            </NIcon>
+          </template>
+        </NButton>
+      </template>
+      <div>
+        <div class="text-center text-sm font-bold pb-2">
+          {{ $t('webpilot.msg.edit_task_title') }}
+        </div>
+        <NInput v-model:value="newTitle" type="textarea" />
+      </div>
+    </NPopconfirm>
     <NButton
       secondary
       circle
@@ -12,17 +58,37 @@
       :disabled="newTask.loading.value"
       @click="newTask.execute()"
     >
-      <div class="i-carbon:add-comment text-lg" />
+      <template #icon>
+        <NIcon>
+          <div class="i-carbon:add-comment" />
+        </NIcon>
+      </template>
     </NButton>
     <NButton v-if="showClose" circle class="!h-8 !w-8" @click="show = false">
-      <div class="i-carbon:close text-lg" />
+      <template #icon>
+        <NIcon>
+          <div class="i-carbon:close" />
+        </NIcon>
+      </template>
     </NButton>
   </div>
 </template>
 
 <script setup lang="ts">
-import { NButton } from 'naive-ui'
+import { NButton, NIcon, NInput, NPopconfirm } from 'naive-ui'
 
 defineProps<{ showClose?: boolean }>()
-const { show, newTask } = useCopilot()
+const { show, newTask, saveTask, summarizeTask, taskContext } = useCopilot()
+const showEditPopup = ref(false)
+const newTitle = ref('')
+
+const toggleEditPopup = () => {
+  showEditPopup.value = true
+  newTitle.value = taskContext.value.title || ''
+}
+
+const applyTitle = () => {
+  taskContext.value.title = newTitle.value
+  saveTask.execute()
+}
 </script>
