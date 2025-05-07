8ab3e2b388bfff5aa71f3176ee3126bd29b07e5f
ytj <tjyan2004@gmail.com>
2025-05-07 10:45:28 +0000

    fix: reorder imports in markdown utility for consistency

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -1,9 +1,9 @@
-import MarkdownItAsync from 'markdown-it-async'
 import { fromAsyncCodeToHtml } from '@shikijs/markdown-it/async'
-import { codeToHtml } from 'shiki'
 import mk from '@vscode/markdown-it-katex'
-import 'github-markdown-css/github-markdown.css'
+import 'github-markdown-css/github-markdown-light.css'
 import 'katex/dist/katex.min.css'
+import MarkdownItAsync from 'markdown-it-async'
+import { codeToHtml } from 'shiki'
 
 const md = MarkdownItAsync()
 md.use(
